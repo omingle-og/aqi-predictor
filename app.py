db1c300656d2146bebf02d4ae5be3053a4ce6fe1
omingle-og <ingleom81@gmail.com>
2026-02-03 12:29:22 +0530

    third

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import 
+import joblib
 
 st.set_page_config(page_title="AQI Prediction App", layout="centered")
 st.title("Air Quality Index (AQI) Prediction")
